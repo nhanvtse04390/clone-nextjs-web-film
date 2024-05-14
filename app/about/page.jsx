7e9bd795e7eea3864255db72b6dff343f21defcd
votrongnhan <nhanvt@mor.com.vn>
2024-05-14 22:42:21 +0700

    create about page

---
+++ app/about/page.jsx
@@ -0,0 +1,84 @@
+export default function About() {
+    return (
+        <div className='max-w-6xl mx-auto p-3 space-y-4'>
+            <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+            <p>
+                To be honest, I could not find anyone else on YouTube who does not copy and paste or watch a second
+                monitor while presenting themselves as a super developer. Sahand is truly exceptional; he has taught me
+                what development looks like. His videos are recorded naturally, as he looks at Figma and writes code
+                like a real developer. Sahand is undeniably one of the best YouTubers I have ever seen.
+                Thank you, Sahand, for your continuous dedication. You truly deserve thousands of subscribers. Your
+                skills as a developer are truly remarkable.To be honest, I couldn't find anyone else on YouTube who does
+                not copy and paste or watch a second monitor while presenting themselves as a super developer. Sahand is
+                truly exceptional; he has taught me what development looks like. His videos are recorded naturally, as
+                he looks at Figma and writes code like a real developer. Sahand is undeniably one of the best YouTubers
+                I have ever seen.
+                Thank you, Sahand, for your continuous dedication. You truly deserve thousands of subscribers. Your
+                skills as a developer are truly remarkable.To be honest, I couldn't find anyone else on YouTube who does
+                not copy and paste or watch a second monitor while presenting themselves as a super developer. Sahand is
+                truly exceptional; he has taught me what development looks like. His videos are recorded naturally, as
+                he looks at Figma and writes code like a real developer. Sahand is undeniably one of the best YouTubers
+                I have ever seen.
+                Thank you, Sahand, for your continuous dedication. You truly deserve thousands of subscribers. Your
+                skills as a developer are truly remarkable.To be honest, I couldn't find anyone else on YouTube who does
+                not copy and paste or watch a second monitor while presenting themselves as a super developer. Sahand is
+                truly exceptional; he has taught me what development looks like. His videos are recorded naturally, as
+                he looks at Figma and writes code like a real developer. Sahand is undeniably one of the best YouTubers
+                I have ever seen.
+            </p>
+            <p>
+                Thank you, Sahand, for your continuous dedication. You truly deserve thousands of subscribers. Your
+                skills as a developer are truly remarkable.To be honest, I couldn't find anyone else on YouTube who does
+                not copy and paste or watch a second monitor while presenting themselves as a super developer. Sahand is
+                truly exceptional; he has taught me what development looks like. His videos are recorded naturally, as
+                he looks at Figma and writes code like a real developer. Sahand is undeniably one of the best YouTubers
+                I have ever seen.
+                Thank you, Sahand, for your continuous dedication. You truly deserve thousands of subscribers. Your
+                skills as a developer are truly remarkable.To be honest, I couldn't find anyone else on YouTube who does
+                not copy and paste or watch a second monitor while presenting themselves as a super developer. Sahand is
+                truly exceptional; he has taught me what development looks like. His videos are recorded naturally, as
+                he looks at Figma and writes code like a real developer. Sahand is undeniably one of the best YouTubers
+                I have ever seen.
+                Thank you, Sahand, for your continuous dedication. You truly deserve thousands of subscribers. Your
+                skills as a developer are truly remarkable.To be honest, I couldn't find anyone else on YouTube who does
+                not copy and paste or watch a second monitor while presenting themselves as a super developer. Sahand is
+                truly exceptional; he has taught me what development looks like. His videos are recorded naturally, as
+                he looks at Figma and writes code like a real developer. Sahand is undeniably one of the best YouTubers
+                I have ever seen.
+                Thank you, Sahand, for your continuous dedication. You truly deserve thousands of subscribers. Your
+                skills as a developer are truly remarkable.To be honest, I couldn't find anyone else on YouTube who does
+                not copy and paste or watch a second monitor while presenting themselves as a super developer. Sahand is
+                truly exceptional; he has taught me what development looks like. His videos are recorded naturally, as
+                he looks at Figma and writes code like a real developer. Sahand is undeniably one of the best YouTubers
+                I have ever seen.
+            </p>
+            <p> Thank you, Sahand, for your continuous dedication. You truly deserve thousands of subscribers. Your
+                skills as a developer are truly remarkable.To be honest, I couldn't find anyone else on YouTube who
+                does not copy and paste or watch a second monitor while presenting themselves as a super developer.
+                Sahand is truly exceptional; he has taught me what development looks like. His videos are recorded
+                naturally, as he looks at Figma and writes code like a real developer. Sahand is undeniably one of
+                the best YouTubers I have ever seen.
+                Thank you, Sahand, for your continuous dedication. You truly deserve thousands of subscribers. Your
+                skills as a developer are truly remarkable.To be honest, I couldn't find anyone else on YouTube who
+                does not copy and paste or watch a second monitor while presenting themselves as a super developer.
+                Sahand is truly exceptional; he has taught me what development looks like. His videos are recorded
+                naturally, as he looks at Figma and writes code like a real developer. Sahand is undeniably one of
+                the best YouTubers I have ever seen.
+                Thank you, Sahand, for your continuous dedication. You truly deserve thousands of subscribers. Your
+                skills as a developer are truly remarkable.To be honest, I couldn't find anyone else on YouTube who
+                does not copy and paste or watch a second monitor while presenting themselves as a super developer.
+                Sahand is truly exceptional; he has taught me what development looks like. His videos are recorded
+                naturally, as he looks at Figma and writes code like a real developer. Sahand is undeniably one of
+                the best YouTubers I have ever seen.
+                Thank you, Sahand, for your continuous dedication. You truly deserve thousands of subscribers. Your
+                skills as a developer are truly remarkable.To be honest, I couldn't find anyone else on YouTube who
+                does not copy and paste or watch a second monitor while presenting themselves as a super developer.
+                Sahand is truly exceptional; he has taught me what development looks like. His videos are recorded
+                naturally, as he looks at Figma and writes code like a real developer. Sahand is undeniably one of
+                the best YouTubers I have ever seen.
+                Thank you, Sahand, for your continuous dedication. You truly deserve thousands of subscribers. Your
+                skills as a developer are truly remarkable.
+            </p>
+        </div>
+    );
+}
