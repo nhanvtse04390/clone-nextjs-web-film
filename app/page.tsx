f5b0e8d924ba665ba0d79795a51174582e4403da
votrongnhan <nhanvt@mor.com.vn>
2024-05-15 23:14:12 +0700

    new feat

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,19 @@
-export default function Home() {
+const API_KEY = process.env.API_KEY
+
+// @ts-ignore
+export default async function Home({searchParams}) {
+    const genre = searchParams?.genre || 'fetchTrending'
+    const res = await fetch(
+        `https://api.themoviedb.org/3${
+            genre === 'fetchToprated' ? `/movie/top_rated` : `/trending/all/week`
+        }?api_key=${API_KEY}&language=en-US&page=1`
+    )
+    const data = await res.json();
+    if(!res.ok){
+        throw new Error('Failed to fetch data')
+    }
+    const results= data.results
+    console.log("results",results)
     return (
         <div>Home</div>
     )
